445ae4230651db53b4783dfc77969139536c994b
Felipe Augusto Oliveira Dantas <felipedantas184@gmail.com>
2024-12-21 18:46:13 -0300

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TH2WHCRX5C"></Script>
+    <Script id='google-analytics' >
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-TH2WHCRX5C');
+        `}
+    </Script>
+    <Component {...pageProps} />
+  </>;
 }
